d3fdc2677243fb6b579b01bc4d7572c3177ad48d
henriette-bizy <hopebiziyaremye@gmail.com>
2022-11-08 23:31:15 +0200

    navbar issue solved , completion of navbar on 80%

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -1,20 +1,20 @@
 <template>
-<div class="text-white flex">
-    <div class="logo text-lg">
-<h1 class="text-lg ">Me.</h1>
+<div class=" text-white flex flex-row ">
+    <div class="basis-1/4 pb-[10px] mt-7">
+<h1 class="text-[26px] font-bold ">Me.</h1>
 </div>
-<div class="links">
-    <ul>
-<li class="text-8xl font-bold">Home</li>
-<li>About</li>
-<li>Skills</li>
-<li>Projects</li>
+<div class="basis-1/2  mt-9 pl-24 pr-88 ">
+    <ul class="flex ">
+<li class="mr-24">Home</li>
+<li class="mr-24">About</li>
+<li class="mr-24">Skills</li>
+<li class="mr-24">Projects</li>
 </ul>
 </div>
 
 
-<div class="button">
-<button>Contact me</button>
+<div class=" basis-1/4 pl-[220px] mt-9">
+<button class="bg-orange -500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Contact me</button>
 </div>
 </div>
 
